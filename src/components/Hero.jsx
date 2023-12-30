55d94076b5b9795a507f0ceb7c92df6b475dbcfd
Zatey <michael.zatey@kibo.school>
2023-12-30 15:02:51 +0000

    add logo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Logo from '../assets/MovieSearch.jpg'
 
 const Hero = () => {
   return (
-    <div>
+    <div className='hero'>
+      <img src={Logo} alt=""  width={120} id='logo'/>
      <div class="container">
   <div>
     <div class="content">
